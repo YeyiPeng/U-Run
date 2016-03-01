8c3d81717194823eb8179c90b1f226fb61b8e11c
YeyiPeng <Yeyi@dhcp-10-105-100-223.wireless.northwestern.private>
2016-02-29 20:33:41 -0600

    fix page jumps

--- log_in.js
+++ log_in.js
@@ -5,7 +5,7 @@ function login() {
 	if (username == "admin1" && password == "123456") {
 		window.location.href = "homepage.html";
 	} else if (username == "admin2" && password == "123456") {
-		window.location.href = "contacts-sponsor.html";
+		window.location.href = "project.html";
 	} else if (username == "admin3" && password == "123456") {
 		window.location.href = "contacts-sponsor.html";
 	} else {
